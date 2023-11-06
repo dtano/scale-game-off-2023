727e15589dc9015320bdf06867f158e1f1a2ba6d
Denzel Joaquin Tano <denzeltano17@gmail.com>
2023-11-06 20:20:36 +0700

    Implement tweening of elevator tablet

--- Assets/Scripts/UI/ElevatorManagement/ElevatorManagementTablet.cs
+++ Assets/Scripts/UI/ElevatorManagement/ElevatorManagementTablet.cs
@@ -12,11 +12,13 @@ public class ElevatorManagementTablet : UIElement
     [SerializeField] private ElevatorPassengerInteractionModal _elevatorPassengerInteractionModal;
 
     private bool _isOn = false;
+    private Vector3 _restingPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        TurnOff();
+        TurnOff(shouldSlide: false);
+        _restingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -25,20 +27,45 @@ public class ElevatorManagementTablet : UIElement
         
     }
 
-    private void TurnOn()
+    private void TurnOn(bool shouldSlide = true)
     {
         _isOn = true;
 
+        if (!shouldSlide)
+        {
+            ShowAllComponents();
+        }
+        else
+        {
+            LeanTween.moveY(gameObject, 360, 0.5f).setOnComplete(ShowAllComponents);
+        }
+    }
+
+    private void ShowAllComponents()
+    {
         _elevatorSelectComponent.Show();
         _elevatorInfoComponent.Show();
         _elevatorPassengerList.Show();
         _elevatorPassengerInteractionModal.Show();
     }
 
-    private void TurnOff()
+    private void TurnOff(bool shouldSlide = true)
     {
         _isOn = false;
 
+        if (!shouldSlide)
+        {
+            HideAllComponents();
+        }
+        else
+        {
+            HideAllComponents();
+            LeanTween.moveY(gameObject, -191, 0.5f);
+        }
+    }
+
+    private void HideAllComponents()
+    {
         _elevatorSelectComponent.Hide();
         _elevatorInfoComponent.Hide();
         _elevatorPassengerList.Hide();
